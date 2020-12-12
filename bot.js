6dac48322a6a5baca28ccab5b573c94a21656438
BartDebeuckelaere <48215837+BartDebeuckelaere@users.noreply.github.com>
2020-12-12 23:05:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,25 +28,7 @@ var lindsayzinnen = [
     "De persoon die we serieus nemen wordt herkend aan de gele strik en roze hoed en deze week is dat Lindsay Deroo! Proficiat Lindsay!"
 ]
 
-var tuurzinnen = [
-    "T was Phaedra's idee om jou te pesten, sorry Tuur...",
-    "Thierry, Thierry, Thierry,...",
-    "We all lov u Tuur",
-    "Wnr Avalon?",
-    "Mag k het nr van uw zus?",
-    "Hoe is t met uw zus, Tuur?",
-    "Doet de groetjes aan uw zus he Tuur",
-    "Ej Tuur, is dat ondertussen al af tussen uw zus en diene loser?",
-    "You are awarded the presidential medal of honor for you contribution in Operatie Dubbeldooier",
-    "Allee Tuur altublieft he zeg... Aspis zijn echt wel te jong. VIEZERIK!",
-    "Onnozeleir...",
-    "Weete nog Tuur? Van die keer met Maikal Jakson?",
-    "Da was een vre avond he. Met die tjoepen en die nappen en expirimenteren...",
-    "zou je nie beter stoppen met studeren en full-time Hello-Fresh doen?",
-    "We zijn blij dat je er bent, Tuur. We houden van je zoals jij bent.",
-    "Sorry Tuur... Ik moest jou pesten van Phaedra (grof he?!)",
-    "jemoeder"
-]
+
 var randomZinnen = [
 
     "davey is gey",
