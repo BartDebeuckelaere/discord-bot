c5babd41763617f2a00288fe1f71c93a1d7eda7e
BartDebeuckelaere <48215837+BartDebeuckelaere@users.noreply.github.com>
2020-12-29 10:13:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -148,6 +148,9 @@ function gotMessage(msg){
     if(msg.content === "bot klep toe"){
         zwijg = true
     }
+    if(msg.content === "test"){
+        console.log(client.name)
+    }
     if(msg.content === "Als je flink bent, mag je terug praten. Zal da gaan?"){
         zwijg = false
         msg.channel.send("sorry... t zal niet meer gebeuren...")
